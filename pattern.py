a560a058665e1d36db76e37feb3a01afd18abb87
JinoJoseph,Ai <jinojoseph.ai@gmail.com>
2026-03-14 17:01:49 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,3 +1,8 @@
-for i in range(1,6,1):
-    print(i*"*")
-    i+=1
+
+def pattern(row):
+    for i in  range(1,row+1):
+        print(i*"* ")
+row=int(input(" enter number of rows"))   
+pattern(row)    
+
+
